e5f0c7f8beeae7ff92f6ccb8f32ca935a5822192
ProgrammingHero1 <mamun2k9kuet@gmail.com>
2023-08-21 17:40:52 +0600

    added last part of the code

--- script/calculation.js
+++ script/calculation.js
@@ -3,166 +3,139 @@
 /* Product-1 */
 function calculatePriceOfProduct1() {
   const productPrice1 = getInputValue("product-price-1");
-  console.log(productPrice1);
   let previousPrice1 = getInputValue("total-price");
-  console.log(previousPrice1);
   previousPrice1 = productPrice1 + previousPrice1;
-  console.log(previousPrice1);
-  const totalProductPrice1 = setElementInnerText("total-price", previousPrice1);
+  const totalProductPrice1 = setElementInnerText(
+    "total-price",
+    previousPrice1.toFixed(2)
+  );
   addProductName("add-product-name", "product-name-1");
-
   const makePurchaseButton = document.getElementById("make-purchase-button");
   if (previousPrice1 > 0) {
     makePurchaseButton.disabled = false;
   }
-
   const myButton = document.getElementById("apply-button");
-  if (previousPrice1 >= 100) {
+  if (previousPrice1 >= 200) {
     myButton.disabled = false;
   }
-
-  // return previousPrice1;
 }
 
 /* Product-2 */
 function calculatePriceOfProduct2() {
   const productPrice2 = getInputValue("product-price-2");
-  console.log(productPrice2);
   let previousPrice2 = getInputValue("total-price");
-  console.log(previousPrice2);
   previousPrice2 = productPrice2 + previousPrice2;
-  console.log(previousPrice2);
-  const totalProductPrice2 = setElementInnerText("total-price", previousPrice2);
+  const totalProductPrice2 = setElementInnerText(
+    "total-price",
+    previousPrice2.toFixed(2)
+  );
   addProductName("add-product-name", "product-name-2");
-
   const makePurchaseButton = document.getElementById("make-purchase-button");
   if (previousPrice2 > 0) {
     makePurchaseButton.disabled = false;
   }
-
   const myButton = document.getElementById("apply-button");
-  if (previousPrice2 >= 100) {
+  if (previousPrice2 >= 200) {
     myButton.disabled = false;
   }
-
-  return totalProductPrice2;
 }
+
 /* Product-3 */
 function calculatePriceOfProduct3() {
   const productPrice3 = getInputValue("product-price-3");
-  console.log(productPrice3);
   let previousPrice3 = getInputValue("total-price");
-  console.log(previousPrice3);
   previousPrice3 = productPrice3 + previousPrice3;
-  console.log(previousPrice3);
-  const totalProductPrice3 = setElementInnerText("total-price", previousPrice3);
+  const totalProductPrice3 = setElementInnerText(
+    "total-price",
+    previousPrice3.toFixed(2)
+  );
   addProductName("add-product-name", "product-name-3");
-
   const makePurchaseButton = document.getElementById("make-purchase-button");
   if (previousPrice3 > 0) {
     makePurchaseButton.disabled = false;
   }
-
   const myButton = document.getElementById("apply-button");
-  if (previousPrice3 >= 100) {
+  if (previousPrice3 >= 200) {
     myButton.disabled = false;
   }
-
-  return totalProductPrice3;
 }
+
 /* Product-4 */
 function calculatePriceOfProduct4() {
   const productPrice4 = getInputValue("product-price-4");
-  console.log(productPrice4);
   let previousPrice4 = getInputValue("total-price");
-  console.log(previousPrice4);
   previousPrice4 = productPrice4 + previousPrice4;
-  console.log(previousPrice4);
-  const totalProductPrice4 = setElementInnerText("total-price", previousPrice4);
+  const totalProductPrice4 = setElementInnerText(
+    "total-price",
+    previousPrice4.toFixed(2)
+  );
   addProductName("add-product-name", "product-name-4");
-
   const makePurchaseButton = document.getElementById("make-purchase-button");
   if (previousPrice4 > 0) {
     makePurchaseButton.disabled = false;
   }
-
   const myButton = document.getElementById("apply-button");
-  if (previousPrice4 >= 100) {
+  if (previousPrice4 >= 200) {
     myButton.disabled = false;
   }
-
-  return totalProductPrice4;
 }
+
 /* Product-5 */
 function calculatePriceOfProduct5() {
   const productPrice5 = getInputValue("product-price-5");
-  console.log(productPrice5);
   let previousPrice5 = getInputValue("total-price");
-  console.log(previousPrice5);
   previousPrice5 = productPrice5 + previousPrice5;
-  console.log(previousPrice5);
-  const totalProductPrice5 = setElementInnerText("total-price", previousPrice5);
+  const totalProductPrice5 = setElementInnerText(
+    "total-price",
+    previousPrice5.toFixed(2)
+  );
   addProductName("add-product-name", "product-name-5");
-
   const makePurchaseButton = document.getElementById("make-purchase-button");
   if (previousPrice5 > 0) {
     makePurchaseButton.disabled = false;
   }
-
   const myButton = document.getElementById("apply-button");
-  if (previousPrice5 >= 100) {
+  if (previousPrice5 >= 200) {
     myButton.disabled = false;
   }
-
-  return totalProductPrice5;
 }
+
 /* Product-6 */
 function calculatePriceOfProduct6() {
   const productPrice6 = getInputValue("product-price-6");
-  console.log(productPrice6);
-  let previousPrice6 = getInputValue("total-price");
-  console.log(previousPrice6);
-  previousPrice6 = productPrice6 + previousPrice6;
-  console.log(previousPrice6);
-  const totalProductPrice6 = setElementInnerText("total-price", previousPrice6);
-  addProductName("add-product-name", "product-name-6");
 
+  let previousPrice6 = getInputValue("total-price");
+  previousPrice6 = productPrice6 + previousPrice6;
+  const totalProductPrice6 = setElementInnerText(
+    "total-price",
+    previousPrice6.toFixed(2)
+  );
+  addProductName("add-product-name", "product-name-6");
   const makePurchaseButton = document.getElementById("make-purchase-button");
   if (previousPrice6 > 0) {
     makePurchaseButton.disabled = false;
   }
-
   const myButton = document.getElementById("apply-button");
-  if (previousPrice6 >= 100) {
+  if (previousPrice6 >= 200) {
     myButton.disabled = false;
   }
-
-  return totalProductPrice6;
-}
-// Function for Make Purchase
-function homeBack() {
-  return;
 }
 
 // Function for apply coupon
 function coupon() {
   const couponCode = inputText("input-coupon-field");
   const totalAmount = getInputValue("total-price");
-  console.log(couponCode);
   if (couponCode === "SELL200") {
     const discount = (totalAmount * 0.2).toFixed(2);
     const totalAmountPrice = (totalAmount * 0.8).toFixed(2);
     const discountTextField = document.getElementById("discount-price");
-    console.log(discount);
     discountTextField.innerText = discount;
     const discountAfterPrice = document.getElementById("price-after-discount");
     discountAfterPrice.innerText = totalAmountPrice;
-    console.log(totalAmountPrice);
   }
 }
 
 // function for input value in number
-
 function getInputValue(fieldId) {
   const inputField = document.getElementById(fieldId);
   const inputValueText = inputField.innerText;
@@ -178,20 +151,16 @@ function inputText(textId) {
 }
 
 // Add product name
-
 function addProductName(productId, productNameId) {
-  console.log("productName");
   const productNameEntry = document.getElementById(productId);
   const productNameText = document.getElementById(productNameId);
   const productItemName = productNameText.innerText;
   const p = document.createElement("p");
-
   const count = productNameEntry.childElementCount;
   p.classList.add("text-xl", "font-medium");
   p.innerHTML = `${count + 1}.
   ${productItemName}
   `;
-
   productNameEntry.appendChild(p);
 }
 
@@ -201,7 +170,6 @@ function setElementInnerText(elementId, totalPrice) {
 }
 
 // function to reset the data and navigate to the home page
-
 function resetDataAndGoHome() {
   // Reset the total price
   setElementInnerText("total-price", 0.0);
@@ -213,7 +181,7 @@ function resetDataAndGoHome() {
   // Disable the apply button
   const myButton = document.getElementById("apply-button");
   myButton.disabled = true;
-  // Disable the apply button
+  // Disable the make purchase button
   const makeButton = document.getElementById("make-purchase-button");
   makeButton.disabled = true;
 
